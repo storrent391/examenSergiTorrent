50e32ffe96c03d0af9daaf5ed8b6b0e926d3dc89
storrent391 <storrent391@boscdelacoma.cat>
2024-10-30 20:05:13 +0100

    commit segon intent llegir fitxer

--- examen.js
+++ examen.js
@@ -1,29 +1,16 @@
 const fs = require('fs');
 
-const data = fs.readFileSync('productes.json', 'utf-8');
-
-const productes = JSON.parse(data);
-
-
-function llegirProductes (arrayProductes) {
-
-    for (let i = 0; i < arrayProductes.length; i++) {
-        const producte = arrayProductes[i];
-        console.log(`ID: ${producte.id},Marca: ${producte.marca}, Nom: ${producte.nom}, Preu: ${producte.preu}`);
-    }
 
 
 
+function llegirProductes () {
+
+    const data = fs.readFileSync('productes.json', 'utf-8');
+
+    const productes = JSON.parse(data);
+
+
+    return productes;
 }
-console.log(llegirProductes(productes));
-"id": 1,
-        "nom": "Portàtil",
-        "marca": "Dell",
-        "categoria": "Electrònica",
-        "preu": 850,
-        "disponible": true,
-        "caracteristiques": {
-            "memoria": "16GB",
-            "disc_dur": "512GB SSD",
-            "processador": "Intel i7"
-        }
+
+productes = llegirProductes();
