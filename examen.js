243dafd98ec8b5ac171f7c97d79229fc38a478d6
storrent391 <storrent391@boscdelacoma.cat>
2024-10-30 20:19:30 +0100

    commit 20:20

--- examen.js
+++ examen.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 
 
-
-
 function llegirProductes () {
 
     const data = fs.readFileSync('productes.json', 'utf-8');
@@ -13,4 +11,17 @@ function llegirProductes () {
     return productes;
 }
 
+let productes = [];
 productes = llegirProductes();
+
+function mostrarProducte (producte){
+
+}
+
+function mostrarProductes (arrayProductes) {
+    for (let i = 0; i < arrayProductes.length; i++) {
+        const producte = arrayProductes[i];
+        console.log(`ID: ${producte.id},Marca: ${producte.marca}, Nom: ${producte.nom}, Preu: ${producte.preu}, Disponible: ${producte.disponible}}`);
+    }
+}
+mostrarProductes(productes);
