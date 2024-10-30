d2f5e2d99beb908ec4e00b72ba1a2260607d90c6
storrent391 <storrent391@boscdelacoma.cat>
2024-10-30 20:01:46 +0100

    commit 20:00

--- examen.js
+++ examen.js
@@ -9,10 +9,21 @@ function llegirProductes (arrayProductes) {
 
     for (let i = 0; i < arrayProductes.length; i++) {
         const producte = arrayProductes[i];
-        console.log(`ID: ${producte.id}, Nom: ${producte.nom}, Preu: ${producte.preu}`);
+        console.log(`ID: ${producte.id},Marca: ${producte.marca}, Nom: ${producte.nom}, Preu: ${producte.preu}`);
     }
 
 
 
 }
 console.log(llegirProductes(productes));
+"id": 1,
+        "nom": "Portàtil",
+        "marca": "Dell",
+        "categoria": "Electrònica",
+        "preu": 850,
+        "disponible": true,
+        "caracteristiques": {
+            "memoria": "16GB",
+            "disc_dur": "512GB SSD",
+            "processador": "Intel i7"
+        }
